24783feb987557f4715dc5d116a39cf54e019c3d
Samuele Maurizi <samuelemaurizi@gmail.com>
2018-11-04 08:49:15 +0100

    update config

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ module.exports = {
   ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || "http://localhost:3000",
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    "mongodb://rest-api:rest123@ds145083.mlab.com:45083/costumers-api",
+  MONGODB_URI: process.env.MONGODB_URI || "your mongodb URI",
   JWT_SECRET: process.env.JWT_SECRET || "secret"
 };
